ccdb6dcec9c501d3d40fddc3d28392a613e05c66
Trần Hoài An <hoaian2611an@gmail.com>
2026-01-10 09:03:36 +0700

    Sửa connection string

--- PBL3Context.cs
+++ PBL3Context.cs
@@ -1,11 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using System.Linq; // Thêm cái này để dùng được hàm .Any()
 
 namespace PBL3_Project
 {
     public class PBL3Context : DbContext
     {
-        // Tên DbSet nên đặt số nhiều (Categories, Products) 
-        // để phân biệt với tên Class (Category, Product)
         public DbSet<Category> Category { get; set; }
         public DbSet<Product> Product { get; set; }
         public DbSet<Employee> Employee { get; set; }
@@ -13,13 +12,11 @@ namespace PBL3_Project
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // Database=ShopGiayDB để kết nối đúng vào db bro vừa tạo, không vào master nữa
-            optionsBuilder.UseSqlServer(@"Server=MSI\SQLEXPRESS;Database=ShopGiayDB;Trusted_Connection=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ShopGiayDB;Trusted_Connection=True;TrustServerCertificate=True;");
         }
 
         public void SeedData()
         {
-            // Sửa lại theo tên mới: LoaiGiays -> Categories
             if (!Category.Any())
             {
                 Category.Add(new Category { category_name = "Sneaker" });
